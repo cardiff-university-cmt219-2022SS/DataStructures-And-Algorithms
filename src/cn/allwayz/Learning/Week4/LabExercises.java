3f5fccb3e03a6756b830720a6a1e2b5b72eac882
allwayz <allwayz_cn@foxmail.com>
2022-02-18 16:51:18 +0000

    Changes

---
+++ src/cn/allwayz/Learning/Week4/LabExercises.java
@@ -0,0 +1,31 @@
+package cn.allwayz.Learning.Week4;
+
+import java.util.Random;
+
+/**
+ * @author allwayz
+ * @create 2022-02-17 21:56
+ */
+public class LabExercises {
+    public static void main(String[] args) {
+        Random random;
+        for (int i = 0; i < 16; i++) {
+            random = new Random();
+            int j = (random.nextInt(6-3)+1)+3;
+            System.out.println(j);
+        }
+    }
+
+    public void section1(){
+
+    }
+
+    public void section2(){
+
+    }
+
+    public void section3(){
+
+    }
+}
+
