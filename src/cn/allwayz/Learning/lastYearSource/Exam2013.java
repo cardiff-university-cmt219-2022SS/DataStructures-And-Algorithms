b4e0925df308d71050289e5f87f65fabc6dd931f
Yuchen Liu <liuy282@cardiff.ac.uk>
2022-03-01 17:24:33 +0000

    Week 5

---
+++ src/cn/allwayz/Learning/lastYearSource/Exam2013.java
@@ -0,0 +1,19 @@
+package cn.allwayz.Learning.lastYearSource;
+
+/**
+ * @author allwayz
+ * @create 2022-02-28 16:05
+ */
+public class Exam2013 {
+    public static void main(String[] args) {
+        int i = 11;
+        double d = 3.7;
+        double r2 = (double) ((1 / 3) * i + (1 / 2) * d);
+        System.out.println(r2);
+        int r3 = (i / 2) << 2;
+        System.out.println(r3);
+        StringBuffer stringBuffer = new StringBuffer("11 3.7");
+        stringBuffer.replace(2,5,"***");
+        System.out.println(stringBuffer);
+    }
+}
